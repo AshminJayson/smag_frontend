dbe72300d5ad7d622f67600766753a8dbceb236e
notS0LD13R <joelsebin@gmail.com>
2023-09-22 20:28:30 +0530

    feat(Auth) : added authContext

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Image from "next/image";
 
+import { useAuth } from "./Contexts/Context";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Hello World!</h1>
+        </main>
+    );
 }
