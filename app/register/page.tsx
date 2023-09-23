a409d5654c1aa53efb2f5af6ead7335b9810561d
marahim20 <minhajabdulrahim@gmail.com>
2023-09-23 20:10:47 +0530

    feat: UI tweaks to register and login pages

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,26 +1,26 @@
 import RegisterForm from "@/components/RegisterPage/registerForm";
 import Nav from "@/components/nav";
-import { Image } from "@nextui-org/image";
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 const Page = (props: Props) => {
-    return (
-        <main className="flex min-h-screen h-full flex-col items-center justify-center p-10">
-            <Nav />
-            <section className="w-full rounded-lg flex justify-between items-center">
-                <Image
-                    alt="hero-image"
-                    fetchPriority="high"
-                    src="/images/loginPageSVG.svg"
-                    width={500}
-                    height={500}
-                />
-                <RegisterForm />
-            </section>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen h-full flex-col items-center justify-center p-10">
+      <Nav />
+      <section className="w-full rounded-lg flex justify-between items-center px-12">
+        <Image
+          alt="hero-image"
+          priority
+          src="/images/signupPageSVG.svg"
+          width={500}
+          height={500}
+        />
+        <RegisterForm />
+      </section>
+    </main>
+  );
 };
 
 export default Page;
