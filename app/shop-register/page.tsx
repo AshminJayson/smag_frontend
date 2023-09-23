204a67d24dbcf4563be9cf0961aae85dfad40a8e
marahim20 <minhajabdulrahim@gmail.com>
2023-09-23 13:06:33 +0530

    feat: added district dropdown

--- app/shop-register/page.tsx
+++ app/shop-register/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import React, { useEffect, useState } from "react";
 import { Input } from "@nextui-org/input";
 import { Select, SelectItem } from "@nextui-org/select";
 
 type Props = { searchParams?: { [key: string]: string | string[] } };
 
 const Page = (props: Props) => {
-  console.log(props.searchParams);
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedDistrict, setSelectedDistrict] = useState("");
   const states = [
     { label: "Andhra Pradesh", value: "AP" },
     { label: "Kerala", value: "KL" },
@@ -17,55 +19,20 @@ const Page = (props: Props) => {
     { label: "Maharashtra", value: "MH" },
   ];
   const districts = [
-    {
-      key: "AP",
-      districts: [
-        { label: "Anantapur", value: "Anantapur" },
-        { label: "Chittoor", value: "Chittoor" },
-      ],
-    },
-    {
-      key: "KL",
-      districts: [
-        { label: "Alappuzha", value: "Alappuzha" },
-        { label: "Ernakulam", value: "Ernakulam" },
-      ],
-    },
-    {
-      key: "TN",
-      districts: [
-        { label: "Ariyalur", value: "Ariyalur" },
-        { label: "Chengalpattu", value: "Chengalpattu" },
-      ],
-    },
-    {
-      key: "KA",
-      districts: [
-        { label: "Bagalkot", value: "Bagalkot" },
-        { label: "Bangalore Rural", value: "Bangalore Rural" },
-      ],
-    },
-    {
-      key: "TS",
-      districts: [
-        { label: "Adilabad", value: "Adilabad" },
-        { label: "Hyderabad", value: "Hyderabad" },
-      ],
-    },
-    {
-      key: "MH",
-      districts: [
-        { label: "Ahmednagar", value: "Ahmednagar" },
-        { label: "Akola", value: "Akola" },
-      ],
-    },
+    { label: "Malappuram", value: "MLP" },
+    { label: "Kozhikode", value: "KZD" },
+    { label: "Wayanad", value: "WYD" },
+    { label: "Kannur", value: "KNR" },
+    { label: "Kasaragod", value: "KSD" },
+    { label: "Thrissur", value: "TSR" },
   ];
+
   return (
     <div className="flex min-h-screen h-full flex-col items-center justify-font center p-24">
       <h1 className="font-bold text-lg">Register your shop</h1>
       <h2 className="font-normal text-md">Enter your details below</h2>
       <form>
-        <Card className="flex flex-col w-96">
+        <Card className="flex flex-col gap-4 w-96 p-4">
           <Input
             type="text"
             placeholder="Enter the name of your business"
@@ -74,9 +41,29 @@ const Page = (props: Props) => {
             id="shop_name"
             isRequired
           />
-          <Select variant="bordered" label="State" name="state" id="state">
+          <Select
+            variant="bordered"
+            label="State"
+            name="state"
+            id="state"
+            onChange={(e) => setSelectedState(e.target.value)}
+          >
             {states.map((item, index) => (
-              <SelectItem key={`state${index}`} value={item.value}>
+              <SelectItem key={item.label} value={item.value}>
+                {item.label}
+              </SelectItem>
+            ))}
+          </Select>
+          <Select
+            variant="bordered"
+            label="District"
+            name="district"
+            id="district"
+            disabled={selectedState == ""}
+            onChange={(e) => setSelectedDistrict(e.target.value)}
+          >
+            {districts.map((item, index) => (
+              <SelectItem key={item.label} value={item.value}>
                 {item.label}
               </SelectItem>
             ))}
