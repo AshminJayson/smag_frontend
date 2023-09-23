1ed2ce0704740b0dc62adddbb85aa10b73027482
notS0LD13R <joelsebin@gmail.com>
2023-09-23 11:11:30 +0530

    feat(reg-shop) : added file upload

--- app/shop-register/page.tsx
+++ app/shop-register/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import { Button } from "@nextui-org/button";
 
 type Props = { searchParams?: { [key: string]: string | string[] } };
 
@@ -12,6 +13,14 @@ const Page = (props: Props) => {
             <h2 className="font-normal text-md">Enter your details below</h2>
             <form>
                 <Card></Card>
+                <Card className="flex justify-center items-center min">
+                    <Button color="primary" className="p-0">
+                        <label className="grow h-full leading-9 cursor-pointer">
+                            Upload
+                            <input type="file" className="hidden" />
+                        </label>
+                    </Button>
+                </Card>
             </form>
         </div>
     );
